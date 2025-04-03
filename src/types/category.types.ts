302ef6c1b9fa43c1d4725acfabac4f455af7434a
zainab <hlalzynbhlal@gmail.com>
2025-04-03 19:08:20 +0200

    update

--- src/types/category.types.ts
+++
@@ -1,6 +0,0 @@
-export default type TCategory = {
-  id?: number;
-  title: string;
-  prefix: string;
-  img: string;
-};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,7 @@
 import { type TLoading } from "./shared.types";
-import { type TCategory } from "./category.types";
 import { type TProduct } from "./product.types";
 import { type TOrderItem } from "./order.type";
 import { type TToast } from "./toast.type";
 import { isString } from "./guards";
 
-export { TLoading, TCategory, TProduct, TOrderItem, isString, TToast };
+export { TLoading, TProduct, TOrderItem, isString, TToast };
