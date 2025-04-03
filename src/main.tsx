7d253d7d7887ca58f2bf1a7343d2604c1b8ee652
zainab <hlalzynbhlal@gmail.com>
2025-04-03 19:23:07 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import AppRouter from "@routes/AppRouter";
 import { store, persistor } from "@store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import "./services/axios-global.js";
+import "./services/axios-global";
 import "@styles/global.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
